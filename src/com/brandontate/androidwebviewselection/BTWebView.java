9f7539a76d3d70eec20778cd120484eb04b22cbd
Brandon Tate <brandonntate@gmail.com>
2013-08-13 09:19:53 -0400

    Added licensing information.

--- src/com/brandontate/androidwebviewselection/BTWebView.java
+++ src/com/brandontate/androidwebviewselection/BTWebView.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2012 Brandon Tate
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.brandontate.androidwebviewselection;
 
 import android.webkit.WebSettings;
@@ -33,6 +49,11 @@ import android.view.WindowManager;
 import android.webkit.WebView;
 import android.widget.ImageView;
 
+/**
+ * Webview subclass that hijacks web content selection.
+ *
+ * @author Brandon Tate
+ */
 public class BTWebView extends WebView implements TextSelectionJavascriptInterfaceListener,
         OnTouchListener, OnLongClickListener, OnDismissListener, DragListener{
 
--- src/com/brandontate/androidwebviewselection/TextSelectionJavascriptInterfaceListener.java
+++ src/com/brandontate/androidwebviewselection/TextSelectionJavascriptInterfaceListener.java
@@ -1,10 +1,31 @@
+/*
+ * Copyright (C) 2012 Brandon Tate
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.brandontate.androidwebviewselection;
 
+/**
+ * Text Selection Listener Interface
+ *
+ * @author Brandon Tate
+ */
 public interface TextSelectionJavascriptInterfaceListener {
 
 	/**
 	 * Informs the listener that there was a javascript error.
-	 * @param range
+	 * @param error
 	 */
 	public abstract void tsjiJSError(String error);
 	
